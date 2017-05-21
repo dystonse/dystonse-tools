3343fa45d48eeb667ba2ffe7ea600e8f5c2b748f
Lena Schimmel <lena.schimmel@ameno.de>
2017-05-21 18:07:42 +0200

    Showing routes and train data, some restructuring.

--- src/main/java/net/dystonse/tools/Geocode.java
+++ src/main/java/net/dystonse/tools/Geocode.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.ResultSet;
+import java.sql.Timestamp;
+
 
 import java.io.IOException;
 
@@ -11,6 +13,7 @@ import org.apache.commons.cli.*;
 
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -23,6 +26,7 @@ import org.jxmapviewer.OSMTileFactoryInfo;
 import org.jxmapviewer.viewer.DefaultTileFactory;
 import org.jxmapviewer.viewer.DefaultWaypoint;
 import org.jxmapviewer.viewer.GeoPosition;
+import org.jxmapviewer.viewer.TileFactory;
 import org.jxmapviewer.viewer.TileFactoryInfo;
 import org.jxmapviewer.viewer.Waypoint;
 import org.jxmapviewer.viewer.WaypointPainter;
@@ -47,36 +51,92 @@ public class Geocode
  
     public static void main(String[] args) throws SQLException, IOException {
         parseCommandLine(args);
+        //System.in.read();
         bla();
     }
 
-    public static void bla() throws SQLException {
+    private static List<List<GeoPosition>> loadRealtimedata(String routeName) throws SQLException {
+        List<List<GeoPosition>> ret = new LinkedList<List<GeoPosition>>();
+
         Connection conn = Database.getConnection(line);
-        String routeName = line.getOptionValue(optRoute.getOpt());
+         
         System.out.println("Querying data for route " + routeName);
         Statement outerStmt = conn.createStatement();
         Statement innerStmt = conn.createStatement();
-        ResultSet outerRs = outerStmt.executeQuery("SELECT DISTINCT `compound_id` FROM `realtime-input` WHERE `name` = '"+routeName+"' LIMIT 0,30");
+        ResultSet outerRs = outerStmt.executeQuery("SELECT DISTINCT `compound_id` FROM `realtime-input` WHERE `name` = '"+routeName+"' LIMIT 0,10");
         
-    	// Create a track from the geo-positions
-		RoutePainter routePainter = new RoutePainter();
-
+        final int MAX_TIME_DIFF = 1000 * 60 * 3;
 
         while(outerRs.next()) {
             String id = outerRs.getString("compound_id");
             System.out.println("Fetching samples for train " + id + "...");
             List<GeoPosition> track = new ArrayList<GeoPosition>();
 		
-            ResultSet innerRs = innerStmt.executeQuery("SELECT `location` FROM `realtime-input` WHERE `compound_id`='" + id + "' AND `name` = '"+routeName+"' ORDER BY `timestamp`;");
+            ResultSet innerRs = innerStmt.executeQuery("SELECT `location`,`timestamp` FROM `realtime-input` WHERE `compound_id`='" + id + "' AND `name` = '"+routeName+"' ORDER BY `timestamp`;");
+            Timestamp lastTs = null;
             while(innerRs.next()) {
                 Point p = new Point((byte[])innerRs.getObject("location"));
-                System.out.println(p.latitude + ", " + p.longitude);
+                Timestamp ts = innerRs.getTimestamp("timestamp");
+                if(lastTs != null && (ts.getTime () - lastTs.getTime ()) > MAX_TIME_DIFF) {
+                     ret.add(track);
+                     track = new ArrayList<GeoPosition>();
+                }
+
                 track.add(new GeoPosition(p.latitude, p.longitude));
+                lastTs = ts;
             }
             innerRs.close();
-            routePainter.addRoute(track);
+            ret.add(track);
         }
         outerRs.close();     
+        return ret;
+    }
+
+    private static List<List<GeoPosition>> loadShapes(String routeName) throws SQLException {
+        List<List<GeoPosition>> ret = new LinkedList<List<GeoPosition>>();
+
+        Connection conn = Database.getConnection(line);
+         
+        System.out.println("Querying data for route " + routeName);
+        Statement outerStmt = conn.createStatement();
+        Statement innerStmt = conn.createStatement();
+        ResultSet outerRs = outerStmt.executeQuery("SELECT DISTINCT shape_id FROM trips WHERE route_id IN (SELECT route_id FROM routes WHERE route_short_name = '"+routeName+"');");
+        
+        while(outerRs.next()) {
+            String id = outerRs.getString("shape_id");
+            System.out.println("Fetching samples for shape " + id + "...");
+            List<GeoPosition> track = new ArrayList<GeoPosition>();
+		
+            ResultSet innerRs = innerStmt.executeQuery("SELECT shape_pt_sequence, shape_pt_lat, shape_pt_lon FROM shapes WHERE shape_id = '" + id + "' ORDER BY shape_pt_sequence;");
+            while(innerRs.next()) {
+                //Point p = new Point((byte[])innerRs.getObject("location"));
+                double latitude = innerRs.getDouble("shape_pt_lat");
+                double longitude = innerRs.getDouble("shape_pt_lon");
+                track.add(new GeoPosition(latitude, longitude));
+            }
+            innerRs.close();
+            ret.add(track);
+        }
+        outerRs.close();     
+        return ret;
+    }
+
+    public static void bla() throws SQLException {
+        String routeName = line.getOptionValue(optRoute.getOpt());
+        
+        RoutePainter routePainter = new RoutePainter(true, false);
+        RoutePainter trainPainter = new RoutePainter(false, true);
+        for(List<GeoPosition> track : loadRealtimedata(routeName)) {
+            trainPainter.addRoute(track);
+        }
+
+        for(List<GeoPosition> track : loadShapes(routeName)) {
+            routePainter.addRoute(track);
+        }
+
+        // TODO Die obere Struktur mit den zwei Schleifen in Methode auslagern, duplozieren, für Routen anpassen. RoutePainter parametrisieren für Linien, Kreise oder beides.
+        // SELECT DISTINCT shape_id FROM trips WHERE route_id IN (SELECT route_id FROM routes WHERE route_short_name = "S42");
+        // SELECT shape_pt_sequence, shape_pt_lat, shape_pt_lon FROM shapes WHERE shape_id =1119 ORDER BY shape_pt_sequence
 
         JXMapViewer mapViewer = new JXMapViewer();
 
@@ -108,7 +168,7 @@ public class Geocode
 	
 		// Set the focus
 		mapViewer.zoomToBestFit(new HashSet<GeoPosition>(routePainter.getRoute(0)), 0.7);
-
+/*
 		// Create waypoints from the geo-positions
 		Set<Waypoint> waypoints = new HashSet<Waypoint>();
         int i = 0;
@@ -121,11 +181,11 @@ public class Geocode
 		// Create a waypoint painter that takes all the waypoints
 		WaypointPainter<Waypoint> waypointPainter = new WaypointPainter<Waypoint>();
 		waypointPainter.setWaypoints(waypoints);
-		
+		*/
 		// Create a compound painter that uses both the route-painter and the waypoint-painter
 	    List<Painter<JXMapViewer>> painters = new ArrayList<Painter<JXMapViewer>>();
 		painters.add(routePainter);
-		painters.add(waypointPainter);
+		painters.add(trainPainter);
 		
         CompoundPainter<JXMapViewer> painter = new CompoundPainter<JXMapViewer>(painters);
         mapViewer.setOverlayPainter(painter);      
@@ -176,17 +236,19 @@ public class Geocode
 
 class RoutePainter implements Painter<JXMapViewer>
 {
-	private boolean antiAlias = true;
+	private boolean antiAlias = false;
+    private boolean lines = true;
+    private boolean dots = false;
 	
 	private List<List<GeoPosition>> tracks;
 	
 	/**
 	 * @param track the track
 	 */
-	public RoutePainter()
+	public RoutePainter(boolean lines, boolean dots)
 	{
-		// copy the list so that changes in the 
-		// original list do not have an effect here
+        this.lines = lines;
+        this.dots = dots;
 		this.tracks = new ArrayList<List<GeoPosition>>();
 	}
 
@@ -223,7 +285,7 @@ class RoutePainter implements Painter<JXMapViewer>
 
             // do the drawing again
             g.setColor(new Color(Color.HSBtoRGB(((float)i) / 12.0f, 1, 0.5f)));
-            g.setStroke(new BasicStroke(2));
+            g.setStroke(new BasicStroke(3));
 
             drawRoute(g, map, track);
             i++;
@@ -242,23 +304,31 @@ class RoutePainter implements Painter<JXMapViewer>
 		int lastY = 0;
 		
 		boolean first = true;
+        int zoom = map.getZoom();
+        TileFactory tf = map.getTileFactory();
 		
 		for (GeoPosition gp : track)
 		{
 			// convert geo-coordinate to world bitmap pixel
-			Point2D pt = map.getTileFactory().geoToPixel(gp, map.getZoom());
+			Point2D pt = tf.geoToPixel(gp, zoom);
 
-			if (first)
-			{
-				first = false;
-			}
-			else
-			{
-				g.drawLine(lastX, lastY, (int) pt.getX(), (int) pt.getY());
-			}
-			
-			lastX = (int) pt.getX();
-			lastY = (int) pt.getY();
+            if(dots) {
+                g.drawArc((int) pt.getX()-10, (int) pt.getY()-10, 20, 20, 0, 360);
+            }
+
+            if(lines) {
+                if (first)
+                {
+                    first = false;
+                }
+                else
+                {
+                    g.drawLine(lastX, lastY, (int) pt.getX(), (int) pt.getY());
+                }
+                
+                lastX = (int) pt.getX();
+                lastY = (int) pt.getY();
+            }
 		}
 	}
 }
